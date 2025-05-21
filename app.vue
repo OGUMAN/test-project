b0b36bfb0351e2d5c7f294f4ebf802415603fe99
gumanyatko <oleksandrhuman2005@gmail.com>
2025-05-21 22:55:57 +0300

    Locales and app.vue refactor

--- app.vue
+++ app.vue
@@ -1,61 +1,17 @@
 <template>
-  <Title>{{ $t("name") }}</Title>
-  <Meta
-    name="description"
-    content="Join the MOZGI event – an immersive experience with music, innovation, and creativity."
-  />
-  <Meta property="og:title" content="MOZGI Event" />
-  <Meta
-    property="og:description"
-    content="Join the MOZGI event – an immersive experience with music, innovation, and creativity."
-  />
-  <Meta property="og:type" content="website" />
+  <AppSeo />
+
   <Header />
   <main>
     <NuxtPage />
-    <PageTransitionSquare />
   </main>
-</template>
 
-<script lang="ts" setup>
-useHead({
-  link: [
-    {
-      rel: "icon",
-      type: "image/png",
-      href: "/favicon-96x96.png",
-      sizes: "96x96",
-    },
-    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
-    { rel: "shortcut icon", href: "/favicon.ico" },
-    {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      href: "/apple-touch-icon.png",
-    },
-    { rel: "manifest", href: "/site.webmanifest" },
-  ],
-  meta: [{ name: "apple-mobile-web-app-title", content: "Mozgi" }],
-  lang: "en",
-});
-</script>
+  <AppTransition />
+</template>
 
 <style lang="scss">
 @use "./components/header/style.scss" as headerStyle;
 @import url("./assets/styles/main.scss");
-
-body,
-html {
-  background: #bebebe;
-  font-family: "TRYGrtsk", sans-serif;
-  color: #373737;
-}
-
-*,
-*:after,
-*:before {
-  box-sizing: border-box;
-}
 
 main {
   padding-top: headerStyle.$height;
---
+++ components/app/seo.vue
@@ -0,0 +1,30 @@
+<template>
+  <Title>{{ $t("meta.title") }}</Title>
+  <Meta name="description" :content="$t('meta.description')" />
+  <Meta property="og:title" content="MOZGI Event" />
+  <Meta property="og:description" :content="$t('meta.description')" />
+  <Meta property="og:type" content="website" />
+</template>
+
+<script lang="ts" setup>
+useHead({
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-96x96.png",
+      sizes: "96x96",
+    },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    { rel: "shortcut icon", href: "/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
+  ],
+  meta: [{ name: "apple-mobile-web-app-title", content: "Mozgi" }],
+  lang: "en",
+});
+</script>
