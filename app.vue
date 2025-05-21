ad775372428e5e5eb522806c3a3b2ad16b7cca69
gumanyatko <oleksandrhuman2005@gmail.com>
2025-05-21 22:07:57 +0300

    Favicon fix

--- app.vue
+++ app.vue
@@ -19,9 +19,24 @@
 
 <script lang="ts" setup>
 useHead({
-  htmlAttrs: {
-    lang: "en",
-  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-96x96.png",
+      sizes: "96x96",
+    },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    { rel: "shortcut icon", href: "/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
+  ],
+  meta: [{ name: "apple-mobile-web-app-title", content: "Mozgi" }],
+  lang: "en",
 });
 </script>
 
